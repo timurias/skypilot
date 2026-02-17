6a31ed3d72db47781331ec6f42795f2d44538c9b
Timur <timurias.1985@gmail.com>
2026-02-17 10:41:04 +0000

    can you use some real aerial photography for example image (like this ht

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'sovzond.ru',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
